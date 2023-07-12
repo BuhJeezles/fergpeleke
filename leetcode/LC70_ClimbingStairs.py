4a8440bd45b04959a31158c1a38f2f4b3bff7bf7
Will <willjan26@gmail.com>
2023-07-12 17:42:59 -0400

    moved LC problems to leetcode folder

---
+++ leetcode/LC70_ClimbingStairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n):
+
+        def fibonacci(inputNum):
+            # base case: 1 stair
+            if inputNum == 1:
+                return 1
+            # base case: 2 stairs
+            if inputNum == 2:
+                return 2
+
+            ret = fibonacci(inputNum - 1) + fibonacci(inputNum - 2)
+
+            return ret
+
+        
+        return fibonacci(n)
+print('poop')
+test = Solution()
+test1 = test.climbStairs(8)
+print(test1)
