b5e941a640081e4789180e0f5af0a812871064f6
Will <willjan26@gmail.com>
2023-08-02 17:26:47 -0400

    finished MergeSorter.py

--- sorting/MergeSorter.py
+++ sorting/MergeSorter.py
@@ -18,4 +18,39 @@ class MergeSorter(BaseSorter):
 	'''
 
 	def sort(my_list):
-		
+		midpoint = listLen // 2
+
+		# recurse until we have len == 1
+		if len(my_list) <= 1:
+			return
+
+		left = my_list[:midpoint]
+		right = my_list[midpoint:]
+
+		left = sort(left)
+		right = sort(right)
+
+		# combining left and right subsections
+		combinedList = []
+		rightIndex = 0
+		leftIndex = 0
+
+		while True:
+			if leftIndex < len(left) and rightIndex < len(right):
+				if left[leftIndex] < right[rightIndex]:
+					combinedList.append(left[leftIndex])
+					leftIndex += 1
+				else:
+					combinedList.append(right[rightIndex])
+					rightIndex += 1
+			elif leftIndex < len(left):
+				combinedList.append(left[leftIndex])
+			elif rightIndex < len(right):
+				combinedList.append(right[rightIndex])
+			else:
+				return combinedList
+
+
+
+
+
